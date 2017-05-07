c52299d083c0ff2cf8bdf3faa294bce300b18555
NekoGamiYuki <nekogamiyuki@users.noreply.github.com>
2017-05-06 21:49:01 -0700

    Added first step to proper python packaging.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='spicytwitch',
+    version='0.1.0',
+    description='A package for interacting with twitch via IRC. Includes tools for creating a bot.',
+    url='http://github.com/NekoGamiYuki/SpicyTwitch',
+    author='NekoGamiYuki',
+    license='MIT',
+    packages=['spicytwitch'],
+    zip_safe=False
+)
