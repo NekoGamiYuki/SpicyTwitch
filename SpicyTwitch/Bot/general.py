423fdfc09ebccc2eba433915e13c5274349cdaa3
NekoGamiYuki <nekogamiyuki@users.noreply.github.com>
2017-05-06 21:46:35 -0700

    Moved to seperate package/program

--- SpicyTwitch/Bot/general.py
+++
@@ -1,7 +0,0 @@
-"""
-Description:
-A python module for managing the most basic command creation system available, a simple response type command.
-All it needs is a command_name and response
-"""
-
-# NOTE: Maybe I should have this register as a response command and have it manage simple stuff like "!command add"
